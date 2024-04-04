e4050f0903c52bccdbe22b217ced9bf71bf12f08
Dimas Adam Saputra <dimasadam058@gmail.com>
2024-04-04 11:06:31 +0700

    first commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,15 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    <h1 class="text-2xl font-bold">User List</h1>
+                    <ul>
+                        @foreach ($users as $user)
+                            <li class="list-disct">
+                                <a wire:navigate class="link link-primary"
+                                    href="{{ route('chat', $user) }}">{{ $user->name }}</a>
+                            </li>
+                        @endforeach
+                    </ul>
                 </div>
             </div>
         </div>
